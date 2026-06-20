4c969de246c71aee598d0069909654f1729ed9c7
Julio Cezar <juliocps.prof@gmail.com>
2026-06-20 13:56:32 -0300

    Adiciona integração unificada de cursos

---
+++ src/components/CourseUnifiedWorkloadManager.tsx
@@ -0,0 +1,7 @@
+"use client";
+
+import { CourseBuilderWizard } from "@/components/course-studio/CourseBasicsStep";
+
+export function CourseUnifiedWorkloadManager() {
+  return <CourseBuilderWizard />;
+}
