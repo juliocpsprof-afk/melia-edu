5dcb80eaeb90e41901e0c4214098b90a57c82306
Julio Cezar <juliocps.prof@gmail.com>
2026-06-20 13:59:13 -0300

    Remove integração auxiliar não utilizada

--- src/components/CourseUnifiedWorkloadManager.tsx
+++
@@ -1,7 +0,0 @@
-"use client";
-
-import { CourseBuilderWizard } from "@/components/course-studio/CourseBasicsStep";
-
-export function CourseUnifiedWorkloadManager() {
-  return <CourseBuilderWizard />;
-}
